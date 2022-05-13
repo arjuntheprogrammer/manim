de5c100a8c65366f45b1ec219e3de614d54a1c63
Arjun Gupta <arjuntheprogrammer@gmail.com>
2022-05-13 16:27:57 +0530

    added code comments

--- manim/plugins/plugins_flags.py
+++ manim/plugins/plugins_flags.py
@@ -14,12 +14,23 @@ from manim import console
 __all__ = ["list_plugins"]
 
 
+"""
+It returns a dictionary of all the plugins that are installed
+:return: A dictionary of plugins.
+"""
+
+
 def get_plugins():
     plugins = {
         entry_point.name: entry_point.load()
         for entry_point in pkg_resources.iter_entry_points("manim.plugins")
     }
     return plugins
+
+
+'''
+This code is printing a list of all the plugins that are currently loaded
+'''
 
 
 def list_plugins():
